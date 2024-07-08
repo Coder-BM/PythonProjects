c6085fe421751f23e4313008f10313a4382ff0d0
Coder-BM <133743774+Coder-BM@users.noreply.github.com>
2024-07-09 00:19:13 +0530

    While Loop Added

--- qr.py
+++ qr.py
@@ -1,16 +1,25 @@
 import qrcode
+import qrcode.main
 import random
+import time
 
-data = ["Random","Good","Bad","Ugly","Sad"]
-
-rand = random.choice(data)
-
-qr = qrcode.QRCode(version=1,box_size = 10, border=1)
-qr.add_data(rand)
-qr.make(fit=True)
-
-qr_image = qr.make_image(fill_color = "Green", back_color="Black")
-
-qr_image.save("Details.png")
-
-print("Image Saved!!!!")
+counter = 0
+while True:
+    data = ["Random","Good","Bad","Ugly","Sad"]
+    rand = random.choice(data)
+    
+    qr = qrcode.main.QRCode(version=1,box_size=20,border=1)
+    
+    qr.add_data(rand)
+    
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill_color="white",back_color="black")
+    
+    img.save("qrcode.png")
+    
+    time.sleep(10)
+    
+    counter += 1
+    
+    print(f'{counter} : QRSaved')
